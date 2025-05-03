469282037415be105fe746da65c6be9acc7b530c
Артём Власов <aworkprogramm@gmail.com>
2025-05-03 21:13:23 +0500

    added delfilefolder, rnfilefolder, chfilefolder and adfilefolder functionality, changed pkg/git.go, utils/files.go

--- utils/files.go
+++ utils/files.go
@@ -1,64 +1,88 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
 )
 
-func NewFileFolder() ([]string, error) {
+func AdFileFolder() ([]string, error) {
 	cmd := exec.Command("git", "diff", "--cached", "--name-status")
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	var added []string
+	var add []string
 	lines := strings.Split(string(output), "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "A") {
-			added = append(added, strings.TrimSpace(line[2:]))
+			add = append(add, strings.TrimSpace(line[2:]))
 		}
 	}
 
-	return added, nil
+	return add, nil
 }
 
-func DeleteFileFolder() ([]string, error) {
+func DelFileFolder() ([]string, error) {
 	cmd := exec.Command("git", "diff", "--cached", "--name-status")
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	var deleted []string
+	var del []string
 	lines := strings.Split(string(output), "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "D") {
-			deleted = append(deleted, strings.TrimSpace(line[2:]))
+			del = append(del, strings.TrimSpace(line[2:]))
 		}
 	}
 
-	return deleted, nil
+	return del, nil
 }
 
-func RenameFileFolder() ([]string, error) {
+func RnFileFolder() ([]string, error) {
 	cmd := exec.Command("git", "diff", "--cached", "--name-status")
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	var renamed []string
+	var rn []string
 	lines := strings.Split(string(output), "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "R") {
 			parts := strings.Fields(line)
 			oldFileName := parts[1]
 			newFileName := parts[2]
-			renamed = append(renamed, fmt.Sprintf("%s -> %s", oldFileName, newFileName))
+			rn = append(rn, fmt.Sprintf("%s -> %s", oldFileName, newFileName))
 		}
 	}
 
-	return renamed, nil
+	return rn, nil
+}
+
+func ChFileFolder() ([]string, error) {
+	cmd := exec.Command("git", "status", "--porcelain")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	var ch []string
+	lines := strings.Split(out.String(), "\n")
+	for _, line := range lines {
+		if len(line) < 4 {
+			continue
+		}
+		ch = append(ch, strings.TrimSpace(line[3:]))
+	}
+
+	return ch, nil
 }
