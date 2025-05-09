7b11b1ae73409bb2516b4ee0d7a19368651f5da2
Артём Власов <aworkprogramm@gmail.com>
2025-05-09 22:56:58 +0500

    changed the 'fstructure' branch

--- commit.go
+++ commit.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -9,5 +10,7 @@ func Commit(commitMsg string) error {
 	GitLogger(fmt.Sprintf("commit is: %s", commitMsg))
 
 	cmd := exec.Command("git", "commit", "-m", commitMsg)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
