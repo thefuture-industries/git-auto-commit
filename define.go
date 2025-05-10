4d032447a30ca3c22b85bca4f9eb0ee770bfc8bf
Артём Власов <aworkprogramm@gmail.com>
2025-05-10 12:51:54 +0500

    the 'bin/auto-commit' file has been changed

--- define.go
+++ define.go
@@ -1,6 +1,9 @@
 package main
 
+import "time"
+
 const (
-	MAX_LINE_LENGTH   uint16 = 1024
-	MAX_COMMIT_LENGTH uint16 = 300
+	MAX_LINE_LENGTH   uint16        = 1024
+	MAX_COMMIT_LENGTH uint16        = 300
+	COMMIT_TIME       time.Duration = 27 * time.Second
 )
