32f0f2ade07710081a8d1f7aedfade9dab762e4e
Артём Власов <aworkprogramm@gmail.com>
2025-05-10 21:39:48 +0500

    changed the 'v2.0.x-pre' branch

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	if len(os.Args) > 1 && (os.Args[1] == "-w" || os.Args[1] == "--watch") {
-		
+		pathRoot, err := GetGitRoot()
+		if err != nil {
+			ErrorLogger(err)
+			return
+		}
+
+
+
 		WatchCommit("")
 	} else {
 		AutoCommit()
