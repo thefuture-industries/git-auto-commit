c3bd11db543c324ca2c37498008c5fb0bacf7e58
Артём Власов <aworkprogramm@gmail.com>
2025-05-10 22:01:45 +0500

    changed the 'v2.0.x-pre' branch

--- parser.go
+++ parser.go
@@ -78,37 +78,42 @@ func Parser(files []string) (string, error) {
 		}()
 	}
 
+	// for _, file := range files {
+	// 	if uint16(len(payloadMsg)) > MAX_COMMIT_LENGTH {
+	// 		break
+	// 	}
+
+	// 	diff, err := GetDiff(file)
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+
+	// 	lang := DetectLanguage(file)
+	// 	if lang == "" {
+	// 		payloadMsg = appendMsg(payloadMsg, fmt.Sprintf("the '%s' file has been changed", file))
+	// 		continue // README.md, etc.
+	// 	}
+
+	// 	for _, formatted := range []string{
+	// 		FormattedVariables(diff, lang),
+	// 		FormattedFunction(diff, lang),
+	// 		FormattedClass(diff, lang),
+	// 		FormattedLogic(diff, lang),
+	// 		FormattedStruct(diff, lang),
+	// 		FormattedType(diff, lang),
+	// 		FormattedInterface(diff, lang),
+	// 		FormattedEnum(diff, lang),
+	// 	} {
+	// 		if formatted != "" {
+	// 			payloadMsg = appendMsg(payloadMsg, formatted)
+	// 		} // else -> continue
+	// 	}
+	// }
+
 	for _, file := range files {
-		if uint16(len(payloadMsg)) > MAX_COMMIT_LENGTH {
-			break
-		}
-
-		diff, err := GetDiff(file)
-		if err != nil {
-			return "", err
-		}
-
-		lang := DetectLanguage(file)
-		if lang == "" {
-			payloadMsg = appendMsg(payloadMsg, fmt.Sprintf("the '%s' file has been changed", file))
-			continue // README.md, etc.
-		}
-
-		for _, formatted := range []string{
-			FormattedVariables(diff, lang),
-			FormattedFunction(diff, lang),
-			FormattedClass(diff, lang),
-			FormattedLogic(diff, lang),
-			FormattedStruct(diff, lang),
-			FormattedType(diff, lang),
-			FormattedInterface(diff, lang),
-			FormattedEnum(diff, lang),
-		} {
-			if formatted != "" {
-				payloadMsg = appendMsg(payloadMsg, formatted)
-			} // else -> continue
-		}
+		jobs <- file
 	}
+	close(jobs)
 
 	if len(payloadMsg) == 0 {
 		formattedByRemote, err := FormattedByRemote("")
