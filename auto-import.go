d9a96ce0be37e992894fea58afb2abbc967c4530
Артём Власов <aworkprogramm@gmail.com>
2025-05-11 14:26:23 +0500

    the 'auto-commit' file has been changed

--- auto-import.go
+++ auto-import.go
@@ -15,9 +15,7 @@ func FormattedImport(diff, lang, filename string) string {
 	case "python":
 		importRegex = regexp.MustCompile(`^import\s+(\w+)`)
 	case "go":
-		importRegex = regexp.MustCompile(`\"([^\"]+)\"`)
 		inImportBlock := false
-
 		for _, line := range lines {
 			if strings.HasPrefix(line, "+") && len(line) > 1 {
 				trimmed := strings.TrimSpace(line[1:])
@@ -33,28 +31,40 @@ func FormattedImport(diff, lang, filename string) string {
 						continue
 					}
 
-					if m := importRegex.FindStringSubmatch(trimmed); m != nil {
+					if strings.HasPrefix(trimmed, "\"") && strings.HasSuffix(trimmed, "\"") {
+						importName := strings.Trim(trimmed, "\"")
 						builder.Reset()
-						builder.WriteString("include ")
-						builder.WriteString("'")
-						builder.WriteString(m[1])
-						builder.WriteString("'")
-						builder.WriteString(" in ")
+						builder.WriteString("include '")
+						builder.WriteString(importName)
+						builder.WriteString("' in ")
 						builder.WriteString(filename)
 						return builder.String()
 					}
-				} else if m := regexp.MustCompile(`^import\s+\"([^\"]+)\"`).FindStringSubmatch(trimmed); m != nil {
-					builder.Reset()
-					builder.WriteString("include ")
-					builder.WriteString("'")
-					builder.WriteString(m[1])
-					builder.WriteString("'")
-					builder.WriteString(" in ")
-					builder.WriteString(filename)
-					return builder.String()
+
+					// if m := importRegex.FindStringSubmatch(trimmed); m != nil {
+					// 	builder.Reset()
+					// 	builder.WriteString("include ")
+					// 	builder.WriteString("'")
+					// 	builder.WriteString(m[1])
+					// 	builder.WriteString("'")
+					// 	builder.WriteString(" in ")
+					// 	builder.WriteString(filename)
+					// 	return builder.String()
+					// }
+				} else if strings.HasPrefix(trimmed, "import ") {
+					if m := regexp.MustCompile(`^import\s+\"([^\"]+)\"`).FindStringSubmatch(trimmed); m != nil {
+						builder.Reset()
+						builder.WriteString("include '")
+						builder.WriteString(m[1])
+						builder.WriteString("' in ")
+						builder.WriteString(filename)
+						return builder.String()
+					}
 				}
 			}
 		}
+
+		return ""
 	case "javascript", "typescript":
 		importRegex = regexp.MustCompile(`^import\s+.*from\s+['\"]([^'\"]+)['\"]`)
 	case "java":
