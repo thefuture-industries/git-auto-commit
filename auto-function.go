9c9f9609e4b29384d5a69819e648b4c6f55e16a4
Артём Власов <aworkprogramm@gmail.com>
2025-05-11 20:16:54 +0500

    renamed function Test11 -> Test

--- auto-function.go
+++ auto-function.go
@@ -15,7 +15,7 @@ var (
 	functionRegexCSharp    = regexp.MustCompile(`(public|private|protected|internal)?\s*(static)?\s*(\w+)\s+(\w+)\s*\(([^)]*)\)`)
 )
 
-func Test11(name int) {}
+func Test(name int) {}
 func Test1(value int) {}
 
 func ParseToStructureFunction(line, lang string) *types.FunctionSignature {
