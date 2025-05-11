2dd08a145dd10b14e4ab8e7791438988cb5818c1
Артём Власов <aworkprogramm@gmail.com>
2025-05-11 20:14:42 +0500

    added function Test1 | deleted function Test11 | renamed function Test1 -> Test11 | changed parameter in Test1 function

--- auto-function.go
+++ auto-function.go
@@ -15,8 +15,8 @@ var (
 	functionRegexCSharp    = regexp.MustCompile(`(public|private|protected|internal)?\s*(static)?\s*(\w+)\s+(\w+)\s*\(([^)]*)\)`)
 )
 
-func Test11(name string) {}
-func Test1(age int) {}
+func Test11(name int) {}
+func Test1(value int) {}
 
 func ParseToStructureFunction(line, lang string) *types.FunctionSignature {
 	switch lang {
