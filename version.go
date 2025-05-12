1495288732b7502cee52e7d97a08ac92ab2cb0f8
Артём Власов <aworkprogramm@gmail.com>
2025-05-12 23:33:35 +0500

    the 'auto-commit.upx' file has been changed | added variables: versionFile, data | added function GetVersion | deleted function GetVersion | included 'fmt', 'net/http', 'os', 'path/filepath', 'strings' in version.go | the 'auto-commit' file has been changed

--- version.go
+++ version.go
@@ -1,11 +1,41 @@
 package main
 
-func GetVersion(root string) string {
-	versionFile := filepath.Join(gitRoot, ".git", "hooks", "auto-commit.version.txt")
-    version, err := ioutil.ReadFile(versionFile)
-    if err != nil {
-        fmt.Println("Version file not found")
-        return
-    }
-    fmt.Printf("Current version: %s\n", string(version))
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func GetVersion(root string) {
+	versionFile := filepath.Join(root, ".git", "hooks", VERSION_FILE)
+
+	version, err := os.ReadFile(versionFile)
+	if err != nil {
+		ErrorLogger(fmt.Errorf("unknown version for auto-commit, please re-install"))
+		return
+	}
+
+	fmt.Println("[git auto-commit] current version:", string(version))
+
+	resp, err := http.Get(GITHUB_REPO_URL + "/releases/latest")
+	if err != nil {
+		ErrorLogger(fmt.Errorf("could not check latest version"))
+		return
+	}
+	defer resp.Body.Close()
+
+	var data struct {
+		TagName string `json:"tag_name"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		ErrorLogger(fmt.Errorf("could not parse version info"))
+		return
+	}
+
+	if strings.TrimSpace(string(version)) != strings.TrimSpace(data.TagName) {
+		fmt.Println("A new version is available: ", strings.TrimSpace(data.TagName))
+		fmt.Println("Please update! 'git auto -u'")
+	}
 }
