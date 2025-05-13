e32912aaa35e1a42e085425c4ab0e8bc606b3ea1
Артём Власов <aworkprogramm@gmail.com>
2025-05-13 20:31:59 +0500

    the 'update-linux-auto-commit.sh' file has been changed

--- scripts/update-linux-auto-commit.sh
+++ scripts/update-linux-auto-commit.sh
@@ -11,7 +11,10 @@ HOOK_PATH="$HOOKS_DIR/$HOOK_NAME"
 VERSION_FILE="$HOOKS_DIR/auto-commit.version.txt"
 
 if pgrep -f "$HOOK_PATH" > /dev/null; then
-  echo "Stopping running auto-commit processes..."
   pkill -f "$HOOK_PATH"
   sleep 2
 fi
+
+VERSION_URL="https://api.github.com/repos/thefuture-industries/git-auto-commit/releases/latest"
+TAG=$(curl -s "$VERSION_URL" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+URL="https://github.com/thefuture-industries/git-auto-commit/releases/download/$TAG/auto-commit"
