25661e81f927d23ed4100a2de205d1b65a4d4f8c
Артём Власов <aworkprogramm@gmail.com>
2025-05-13 16:18:11 +0500

    changed value in variable buffer | if n is greater than 0; if nw is greater than 0

--- files.go
+++ files.go
@@ -27,7 +27,17 @@ func DownloadBinAutoCommit(url, destPath string) error {
 
 	const progressBarWidth = 70
 	var downloaded int64 = 0
-	buffer := make([]byte, 321025)
+	buffer := make([]byte, 32*1024)
+
+	for {
+		n, err := resp.Body.Read(buffer)
+		if n > 0 {
+			nw, ew := out.Write(buffer[0:n])
+			if nw > 0 {
+				downloaded += int64(nw)
+			}
+		}
+	}
 
 	_, err = io.Copy(out, resp.Body)
 	return err
