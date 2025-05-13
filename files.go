d808195b4d8784530aef88a7f9d0b94ae20dcc60
Артём Власов <aworkprogramm@gmail.com>
2025-05-13 15:23:43 +0500

    added variables: size, downloaded, buffer | added condition logic: size <= 0

--- files.go
+++ files.go
@@ -19,6 +19,16 @@ func DownloadBinAutoCommit(url, destPath string) error {
 	}
 	defer out.Close()
 
+	size := resp.ContentLength
+	if size <= 0 {
+		_, err := io.Copy(out, resp.Body)
+		return err
+	}
+
+	const progressBarWidth = 70
+	var downloaded int64 = 0
+	buffer := make([]byte, 321025)
+
 	_, err = io.Copy(out, resp.Body)
 	return err
 }
