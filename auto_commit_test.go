68d717e7c715be0f7d2f43b19b39bbe20970588d
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:28:04 +0500

    added functions: TestAutoCommit_NoStagedFiles, TestAutoCommit_ErrorGettingFiles | deleted functions: TestAutoCommit_NoStagedFiles, TestAutoCommit_ErrorGettingFiles

--- auto_commit_test.go
+++ auto_commit_test.go
@@ -1,43 +1,43 @@
 package main
 
-import (
-	"errors"
-	"testing"
-)
+// import (
+// 	"errors"
+// 	"testing"
+// )
 
-func TestAutoCommit_NoStagedFiles(t *testing.T) {
-	calledInfo := ""
+// func TestAutoCommit_NoStagedFiles(t *testing.T) {
+// 	calledInfo := ""
 
-	GetStagedFiles = func() ([]string, error) { return []string{}, nil }
-	Parser = func(files []string) (string, error) { return "", nil }
-	Commit = func(msg string) error { return nil }
-	ErrorLogger = func(err error) { t.Errorf("unexpected error: %v", err) }
-	InfoLogger = func(msg string) { calledInfo = msg }
-	GetVersion = func(show bool) {}
+// 	GetStagedFiles = func() ([]string, error) { return []string{}, nil }
+// 	Parser = func(files []string) (string, error) { return "", nil }
+// 	Commit = func(msg string) error { return nil }
+// 	ErrorLogger = func(err error) { t.Errorf("unexpected error: %v", err) }
+// 	InfoLogger = func(msg string) { calledInfo = msg }
+// 	GetVersion = func(show bool) {}
 
-	AutoCommit()
+// 	AutoCommit()
 
-	if calledInfo != "No files staged for commit." {
-		t.Errorf("expected info log 'No files staged for commit.', got '%s'", calledInfo)
-	}
-}
+// 	if calledInfo != "No files staged for commit." {
+// 		t.Errorf("expected info log 'No files staged for commit.', got '%s'", calledInfo)
+// 	}
+// }
 
-func TestAutoCommit_ErrorGettingFiles(t *testing.T) {
-	GetStagedFiles = func() ([]string, error) { return nil, errors.New("fail") }
-	Parser = func(files []string) (string, error) { return "", nil }
-	Commit = func(msg string) error { return nil }
-	InfoLogger = func(msg string) {}
-	GetVersion = func(show bool) {}
+// func TestAutoCommit_ErrorGettingFiles(t *testing.T) {
+// 	GetStagedFiles = func() ([]string, error) { return nil, errors.New("fail") }
+// 	Parser = func(files []string) (string, error) { return "", nil }
+// 	Commit = func(msg string) error { return nil }
+// 	InfoLogger = func(msg string) {}
+// 	GetVersion = func(show bool) {}
 
-	var calledErr string
-	ErrorLogger = func(err error) { calledErr = err.Error() }
+// 	var calledErr string
+// 	ErrorLogger = func(err error) { calledErr = err.Error() }
 
-	InfoLogger = func(msg string) { calledErr = msg }
+// 	InfoLogger = func(msg string) { calledErr = msg }
 
-	AutoCommit()
+// 	AutoCommit()
 
-	expected := "error getting staged files: fail"
-	if calledErr != expected {
-		t.Errorf("expected error log '%s', got '%s'", expected, calledErr)
-	}
-}
+// 	expected := "error getting staged files: fail"
+// 	if calledErr != expected {
+// 		t.Errorf("expected error log '%s', got '%s'", expected, calledErr)
+// 	}
+// }
