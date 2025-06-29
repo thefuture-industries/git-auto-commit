620d3ee80d8bd23adcb378d3ca0503948d729556
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:56:19 +0500

    changed the 'v2.12.x' branch

--- test.g.go
+++ test.g.go
@@ -1,12 +1,23 @@
 package main
 
 type Mocks struct {
-    GetStagedFiles func() ([]string, error)
-    Parser         func([]string) (string, error)
-    Commit         func(string) error
-    ErrorLogger    func(error)
-    InfoLogger     func(string)
-    GetVersion     func(bool)
+	GetStagedFiles func() ([]string, error)
+	Parser         func([]string) (string, error)
+	Commit         func(string) error
+	ErrorLogger    func(error)
+	InfoLogger     func(string)
+	GetVersion     func(bool)
 }
 
-func SaveMocks
+func SaveMocks() *Mocks {
+	return &Mocks{
+		GetStagedFiles: GetStagedFiles,
+		Parser:         Parser,
+		Commit:         Commit,
+		ErrorLogger:    ErrorLogger,
+		InfoLogger:     InfoLogger,
+		GetVersion:     GetVersion,
+	}
+}
+
+func (m *Mocks)
