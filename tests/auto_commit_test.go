a8aadb89fc0bda0bef917480430741253c2c1916
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:05:27 +0500

    added condition logic: calledInfo

--- tests/auto_commit_test.go
+++ tests/auto_commit_test.go
@@ -15,12 +15,15 @@ var (
 
 func TestAutoCommit_NoStagedFiles(t *testing.T) {
 	calledInfo := ""
-	
+
 	getStagedFilesMock = func() ([]string, error) { return []string{}, nil }
 	parserMock = func(files []string) (string, error) { return "", nil }
 	commitMock = func(msg string) error { return nil }
 	errorLoggerMock = func(err error) { t.Errorf("unexpected error: %v", err) }
 	infoLoggerMock = func(msg string) { calledInfo = msg }
+	getVersionMock = func(show bool) {}
 
 	main.AutoCommit()
+
+	if calledInfo
 }
