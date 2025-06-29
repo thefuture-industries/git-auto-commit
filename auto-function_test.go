c2a686799d293914972588c6a718e451916d7761
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:44:50 +0500

    changed the 'v2.12.x' branch

--- auto-function_test.go
+++ auto-function_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestParser_AddedGoFunction(t *testing.T) {
 	GetDiff = func(file string) (string, error) {
-		return "+func TestParser() {}", nil
+		return "+func TestParser()", nil
 	}
 
 	DetectLanguage = func(filename string) string {
