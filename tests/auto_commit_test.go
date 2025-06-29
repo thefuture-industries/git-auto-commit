ee50a1ce0e64864feed8c6628161a3afe5c9d9b3
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:02:40 +0500

    added function TestAutoCommit_NoStagedFiles | included 'testing' in auto_commit_test.go | the 'LGPL-2' file has been changed

---
+++ tests/auto_commit_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"testing"
+)
+
+var (
+	getStagedFilesMock func() ([]string, error)
+	parserMock         func([]string) (string, error)
+	commitMock         func(string) error
+	errorLoggerMock    func(error)
+	infoLoggerMock     func(string)
+	getVersionMock     func(bool)
+)
+
+func TestAutoCommit_NoStagedFiles(t *testing.T) {
+	getStagedFilesMock = func() ([]string, error) { return []string{}, nil }
+
+	main.AutoCommit()
+}
