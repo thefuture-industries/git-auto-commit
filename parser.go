aeb40a3629a9f3acbd3376cb62a0154825f74d10
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 00:40:12 +0500

    changed the 'v2.12.x' branch

--- parser.go
+++ parser.go
@@ -46,7 +46,7 @@ var Parser = func(files []string) (string, error) {
 				mu.Unlock()
 
 				diff, err := GetDiff(file)
-				fmt.
+				fmt.Println(diff)
 				if err != nil {
 					errChan <- fmt.Errorf("error getting diff for %s: %w", file, err)
 					continue
