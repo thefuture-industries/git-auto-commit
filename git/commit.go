25695f0d284fb77c0d4d6a7c9927827ceeecdcfb
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 22:01:28 +0500

    changed the 'v2.12.x' branch

--- git/commit.go
+++ git/commit.go
@@ -2,12 +2,13 @@ package git
 
 import (
 	"fmt"
+	"git-auto-commit/achelper/logger"
 	"os"
 	"os/exec"
 )
 
 var Commit = func(commitMsg string) error {
-	achelper.GitLogger(fmt.Sprintf("commit is: %s", commitMsg))
+	logger.GitLogger(fmt.Sprintf("commit is: %s", commitMsg))
 
 	cmd := exec.Command("git", "commit", "-m", commitMsg)
 	cmd.Stdout = os.Stdout
