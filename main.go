191328c497d79956b3a4091ed5fcfe3bfc00d3ac
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 22:10:11 +0500

    added function AutoCommit | if err is not equal to nil; added condition logic: len(files) == 0; if err is not equal to nil; err := git.Commit(parserMsg); if err is not equal to nil | deleted function AutoCommit

--- main.go
+++ main.go
@@ -31,29 +31,3 @@ func main() {
 		AutoCommit()
 	}
 }
-
-func AutoCommit() {
-	achelper.GetVersion(false)
-
-	files, err := diff.GetStagedFiles()
-	if err != nil {
-		logger.ErrorLogger(fmt.Errorf("error getting staged files: %s", err.Error()))
-		return
-	}
-
-	if len(files) == 0 {
-		logger.InfoLogger("No files staged for commit.")
-		return
-	}
-
-	parserMsg, err := parser.Parser(files)
-	if err != nil {
-		logger.ErrorLogger(err)
-		return
-	}
-
-	if err := git.Commit(parserMsg); err != nil {
-		logger.ErrorLogger(fmt.Errorf("error committing: %s", err.Error()))
-		return
-	}
-}
