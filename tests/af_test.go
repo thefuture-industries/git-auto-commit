f8b1010d34eefca9cd1085d289d136d8bc26b8ec
Артём Власов <aworkprogramm@gmail.com>
2025-06-30 22:22:29 +0500

    added variables: mocks, expected | added functions: TestFormattedFunction_DeletedGoFunctions, DeletedGoFunction | if err is not equal to nil; if msg is not equal to expected

--- tests/af_test.go
+++ tests/af_test.go
@@ -76,6 +76,29 @@ func TestFormattedFunction_DeletedGoFunction(t *testing.T) {
 	}
 }
 
+func TestFormattedFunction_DeletedGoFunctions(t *testing.T) {
+	mocks := SaveMocks()
+	defer mocks.Apply()
+
+	diff.GetDiff = func(file string) (string, error) {
+		return "-func DeletedGoFunction() {}", nil
+	}
+
+	code.DetectLanguage = func(filename string) string {
+		return "go"
+	}
+
+	msg, err := parser.Parser([]string{"auto-commit-parser-test.go"})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := "deleted function DeletedGoFunction"
+	if msg != expected {
+		t.Errorf("expected '%s', got '%s'", expected, msg)
+	}
+}
+
 func TestFormattedFunction_ChangedParamNameGoFunction(t *testing.T) {
 	mocks := SaveMocks()
 	defer mocks.Apply()
