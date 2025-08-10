0506f15bc3be252b24ef27206fe8dbd194b3e908
noneandundefined <aworkprogramm@gmail.com>
2025-08-10 15:55:18 -0400

    Updated infrastructure as code

--- infra/logger/logger.go
+++ infra/logger/logger.go
@@ -2,31 +2,47 @@ package logger
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
-var InfoLogger = func(msg string) {
+var logFile *os.File
+
+func init() {
+	tmpDir := os.TempDir()
+	logPath := filepath.Join(tmpDir, "autocommit.log")
+
+	var err error
+	logFile, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		return
+	}
+}
+
+func logMessage(prefix, msg, color string) {
 	var builder strings.Builder
-	builder.Reset()
+	builder.WriteString(color)
 	builder.WriteString("[git auto-commit] ")
 	builder.WriteString(msg)
-	fmt.Println(builder.String())
+	builder.WriteString("\033[0m\n")
+
+	fmt.Print(builder.String())
+
+	if logFile != nil {
+		fileMsg := fmt.Sprintf("[git auto-commit] %s\n", msg)
+		logFile.WriteString(fileMsg)
+	}
+}
+
+var InfoLogger = func(msg string) {
+	logMessage("", msg, "")
 }
 
 var GitLogger = func(msg string) {
-	var builder strings.Builder
-	builder.Reset()
-	builder.WriteString("\033[0;34m[git auto-commit] ")
-	builder.WriteString(msg)
-	builder.WriteString("\033[0m\n")
-	fmt.Print(builder.String())
+	logMessage("", msg, "\033[0;34m")
 }
 
 var ErrorLogger = func(err error) {
-	var builder strings.Builder
-	builder.Reset()
-	builder.WriteString("\033[0;31m[git auto-commit] ")
-	builder.WriteString(err.Error())
-	builder.WriteString("\033[0m\n")
-	fmt.Print(builder.String())
+	logMessage("", err.Error(), "\033[0;31m")
 }
